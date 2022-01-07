568b255d0f7fd7be4f47c5bbccf3e6cda528fefb
@juan.romeroc <juan.romeroc@infotec.mx>
2022-01-07 10:35:23 -0600

    Push masivo

---
+++ customer/src/main/java/com/paymentchain/customer/entities/Customer.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.paymentchain.customer.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.Data;
+
+/**
+ *
+ * @author sotobotero
+ */
+@Entity
+@Data
+public class Customer {
+   @Id
+  @GeneratedValue(strategy=GenerationType.AUTO)
+   private long id;
+   private String name;
+   private String phone;
+}
